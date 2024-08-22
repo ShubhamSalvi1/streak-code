a8a13e4a1c7484155976a5685a337d39d427f153
ShubhamSalvi <138851412+ShubhamSalvi1@users.noreply.github.com>
2024-08-22 10:53:10 +0530

    Add files via upload

---
+++ f_strings.py
@@ -0,0 +1,6 @@
+#f-strings
+name = "Shubham"
+age = "22"
+print("My name is", name,"and my age is",age) #normal printing os strings or any data type
+print(f"My name is {name} and my age is {age}")#using f-string
+
